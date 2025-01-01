9cf5da58a886d44617ad676e6cfa053494c22b0f
Chris Kowalczyk <61400943+codey-sudo@users.noreply.github.com>
2024-12-31 23:14:24 -0500

    Remove duplicated folder

--- react-book-tracker-app/src/assets/components/Background/Background.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./Background.module.css";
-
-const Background = ({ children }) => {
-  return <div className={style.background}>{children}</div>;
-};
-
-export default Background;
--- react-book-tracker-app/src/assets/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Container } from "./Container/Container";
-export { default as Typography } from "./Typography/Typography";
-export { default as Title } from "./Title/Title";
